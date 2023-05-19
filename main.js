f6992f9ca4977108fcc1236e04e7863ec411a683
WV <wvartech@gmail.com>
2023-05-19 11:12:26 -0500

    Project 2 initial commit

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 let contactos = [
-  { name: "John Doe", email: "john.email" },
-  { name: "Lisa Carbonara", email: "lisacar@gmail.com" },
+  { names: "John", surname:"Doe", phone:404, locations:[{ciudad:"Winsconsin",direccion:"Nose Que"}] , email: "john.email" },
+  { names: "Lisa",surname:"Carbonara", phone:444,locations:[{ciudad:"North Park", direccion:"El Valle"}], email: "lisacar@gmail.com" },
 ];
 
-function AddContact(contact, email) {
-    const exists = contactos.find(c => c.name === contact);
-  if (!exists) {
-    contactos.push({ name: contact, email: email });
+function AddContact(nombres, apellidos, telefono, ubicaciones, email) {
+    const exists = contactos.find(c => c.names === nombres);
+  if (!exists) {    
+    contactos.push({ names: nombres, surname: apellidos, phone: telefono, locations: ubicaciones,  email: email });
   } else {
     console.log("Contact already exists");
   }
 }
 
 function RemoveContact(contact){
-    contactos = contactos.filter(c => c.name !== contact);
+    contactos = contactos.filter(c => c.names !== contact);
 }
 
 function PrintContacts(){
     for(let contact of contactos){
-        console.log("nombre: " + contact.name + " email: " + contact.email);
+
+        console.log("nombre: " + contact.names + " " + contact.surname + " telefono: " + contact.phone + " ubicaciones: " + contact.locations + " email: " + contact.email);
     }
 }
 
 console.log("Starting");
 
-AddContact("Pedro Kabron","pedro@hotmail.com");
-AddContact("Pedro Kabron","pedro@hotmail.com");
+AddContact("Pedro", "Kabron",911,[{ciudad:"Kabronia",direccion:"Calle del arroz"}], "pedro@hotmail.com");
+AddContact("Pedro", "Kabron",911,[{ciudad:"Kabronia",direccion:"Calle del arroz"}], "pedro@hotmail.com");
 RemoveContact("John Doe");
 
 PrintContacts();
