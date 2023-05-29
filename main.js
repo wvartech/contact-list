6d936c233bdfb9418fb8003e24af8c957129b1d3
WV <wvartech@gmail.com>
2023-05-29 10:02:43 -0500

    Rewiew-1 update

--- main.js
+++ main.js
@@ -23,11 +23,28 @@ function PrintContacts(){
     }
 }
 
+function UpdateContact(nombres, apellidos, telefono, ubicaciones, email){
+      const index = contactos.findIndex(c => c.names === nombres);
+  if (index !== -1) {    
+    //contactos.push({ names: nombres, surname: apellidos, phone: telefono, locations: ubicaciones,  email: email });
+    contactos[index].surname = apellidos;
+    contactos[index].phone = telefono;
+    contactos[index].ubicaciones = ubicaciones;
+    contactos[index].email = email;
+  } else {
+    console.log("Contact does not exist.");
+  }
+}
+
 console.log("Starting");
 
 AddContact("Pedro", "Kabron",911,[{ciudad:"Kabronia",direccion:"Calle del arroz"}], "pedro@hotmail.com");
+
+UpdateContact("Pedro", "Velazquez",8190366, [{ciudad:"Bogota",direccion:"El pueblito"}],"pedroVel@gmail.com");
+UpdateContact("Juanito", "Perez",180062344, [{ciudad:"Melcocho",direccion:"El regeton"}],"pereza@hotmail.com");
+
 AddContact("Pedro", "Kabron",911,[{ciudad:"Kabronia",direccion:"Calle del arroz"}], "pedro@hotmail.com");
-RemoveContact("John Doe");
+RemoveContact("John");
 
 PrintContacts();
 
